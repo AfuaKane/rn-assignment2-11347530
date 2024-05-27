d0fee21f5bbe200dbbcef045fbc46ed1e51dd53f
AfuaKane <151206921+AfuaKane@users.noreply.github.com>
2024-05-27 16:25:13 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.boldText}>Gladys Kane</Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#add8e6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
